db127e2eceaedea90de916fd185f303a95b03787
Alex Avitia <alex.avitia77@gmail.com>
2021-05-14 16:29:25 -0500

    working on tests for getting grades

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,9 +1,33 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class StudentTest {
 
     @Test
-    public void testIfIdIsLong() {
-        String expected = "long";
+    public void testForStudent() {
+        Student hank = new Student(1L, "hank");
+
+        assertNotNull(hank);
+    }
+
+    @Test
+    public void testProperties() {
+        Student bob = new Student(1L, "bobby");
+
+        assertEquals(1L, bob.getId());
+        assertEquals("bobby", bob.getName());
+    }
+
+    @Test
+    public void testForAddGrades() {
+        Student peg = new Student(1L, "peggy");
+        peg.addGrade(84);
+        peg.addGrade(70);
+        peg.addGrade(100);
+    }
+
+    @Test
+    public void testForGrades() {
+
     }
 }
